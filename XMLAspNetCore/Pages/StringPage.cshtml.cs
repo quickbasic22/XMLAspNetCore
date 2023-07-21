793215086920c454fc8bdf42de7a652e2f206cac
David Morrow <quickbasic44@outlook.com>
2023-07-20 20:33:10 -0500

    Returned to normal

--- XMLAspNetCore/Pages/StringPage.cshtml.cs
+++ XMLAspNetCore/Pages/StringPage.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel;
@@ -7,20 +8,15 @@ namespace XMLAspNetCore.Pages
 {
     public class StringPageModel : PageModel
     {
-       
         public PageResult OnGet()
         {
             ViewData["Message"] = "Hello World!";
             return Page();
         }
-
         public PageResult OnPostSetStringAsync()
         {
-
             ViewData["Message"] = "OnPostSetString executed!";
             return Page();
         }
-
-
     }
 }
