db5c416c8f86eb0ba8c41a392175f5c1df0b3bea
David Morrow <quickbasic44@outlook.com>
2023-07-22 12:05:19 -0500

    Advertisement Work

---
+++ XMLAspNetCore/Models/XmlContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.DataProtection.Repositories;
+using System.Security.Cryptography.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace XMLAspNetCore.Models
+{
+    public class XmlContext : IXmlRepository
+    {
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {
+            string xmlPath = "C:\\Users\\quick\\source\\repos\\XMLAspNetCore\\XMLAspNetCore\\Pages\\XML\\book.xml";
+            return new XElement[] { XElement.Load(xmlPath) };
+        }
+
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- XMLAspNetCore/Pages/Advertisement/Createcshtml.cs
+++ XMLAspNetCore/Pages/Advertisement/Createcshtml.cs
@@ -8,23 +8,30 @@ namespace XMLAspNetCore.Pages.Advertisement
 {
     public class CreateModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
         [System.ComponentModel.DataAnnotations.Required]
         [StringLength(200)]
         public string AdTitle { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
+        [BindProperty(SupportsGet = true)]
         public string Description { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
+        [BindProperty(SupportsGet = true)]
         public int Duration { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
         public string Condition { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
         [System.ComponentModel.DataAnnotations.Range(typeof(Decimal), "1.00", "1000000.0")]
+        [BindProperty(SupportsGet = true)]
         public decimal Price { get; set; }
         [System.ComponentModel.DataAnnotations.Required]
         [DataType(DataType.EmailAddress)]
+        [BindProperty(SupportsGet = true)]
         public string Email { get; set; }
+        [BindProperty(SupportsGet = true)]
         public string StatusMessage { get; set; }
         public List<SelectListItem> DurationListItems { get; set; }
+        public List<SelectListItem> ConditionListItems { get; set; }
         public void OnGet()
         {
             AdTitle = "1969 Camaro";
@@ -33,18 +40,24 @@ namespace XMLAspNetCore.Pages.Advertisement
             Condition = "Excellent";
             Price = 20000.00M;
             Email = "quickbasic22@yahoo.com";
-            StatusMessage = "OnGet Fired";
+            StatusMessage += "OnGet Fired\n\r";
             DurationListItems = new List<SelectListItem>
             {
-                new SelectListItem { Text = "10", Value = "10" },
-                new SelectListItem { Text = "20", Value = "20" },
-                new SelectListItem { Text = "30", Value = "30" },
-                new SelectListItem { Text = "40", Value = "40" },
+                new SelectListItem { Text = "10", Value = "10", Selected = false },
+                new SelectListItem { Text = "20", Value = "20", Selected = false },
+                new SelectListItem { Text = "30", Value = "30", Selected = true },
+                new SelectListItem { Text = "40", Value = "40", Selected = false },
+            };
+            ConditionListItems = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Fair", Value = "Fair", Selected = true },
+                new SelectListItem { Text = "Good", Value = "Good", Selected = false },
+                new SelectListItem { Text = "As New", Value = "As New", Selected = false },
             };
         }
         public IActionResult OnPostProcess()
         {
-            StatusMessage = "OnPostProcess Fired";
+            StatusMessage += "OnPostProcess Fired\n\r";
             if (ModelState.IsValid)
             {
                 StatusMessage += "\n\rModel is Valid";
